54c06d993ab6e1a3cf91995d4b718bc6267b0f42
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2020-11-12 18:15:31 +0900

    Add files via upload

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -7,46 +7,28 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     title : '',
-    writer : '',
-    password : '',
     contents : null,
     results : [],
+    isLogin : 'Y',
+
+    user:{
+      name:'',
+      email:'',
+      password:'',
+      passwordConfirm:''
+    },
 
     editorOptions:{
       useCommandShortcut:false,
              usageStatistics:true,
-             useDefaultHTMLSanitizer:false, 
+             useDefaultHTMLSanitizer:false, // 아마도 html 태그 관련인듯?
              hideModeSwitch:true, // markdown, wysiwyg 선택하는 스위치
-             toolbarItems:[
-               'heading',
-                'bold',
-                'italic',
-                'strike',
-                'divider',
-                'hr',
-                'quote',
-                'divider',
-                'ul',
-                'ol',
-                'task',
-                'indent',
-                'outdent',
-                'divider',
-                'table',
-                'image',
-                'link',
-                'divider',
-                'code',
-                'codeblock'
-             ],
     }
 
   },
   mutations: {
     allClear(){
       this.state.title='';
-      this.state.writer='';
-      this.state.password='';
       this.state.contents=null;
     },
     successGetList(state,payload){
@@ -54,14 +36,13 @@ export default new Vuex.Store({
     },
     successDetailList(state,payload){
       state.title = payload.title;
-      state.writer = payload.writer;
-      state.password = payload.password;
+      // state.user.name = payload.user.name;
       state.contents = payload.contents;
     }
   },
   actions: {
     getList(context){
-    axios.get('http://localhost:3000/api/getList')
+    axios.get('http://localhost:3000/board/getList')
       .then((res)=>{
         context.commit('successGetList',res.data);
       }).catch(err=>{
@@ -71,16 +52,14 @@ export default new Vuex.Store({
   },
 
     listAdd(context,payload){
-      axios.post('http://localhost:3000/api/listAdd',{
+      axios.post('http://localhost:3000/board/listAdd',{
         title : this.state.title,
-        writer : this.state.writer,
-        password : this.state.password,
         contents : payload        
       })
     },
     detailPage(context,payload){
       console.log('action id :',payload)
-      axios.get(`http://localhost:3000/api/detailList?id=${payload}`,{payload})
+      axios.get(`http://localhost:3000/board/detailList?id=${payload}`,{payload})
       .then((res)=>{
         context.commit('successDetailList',res.data)
       }).catch(err=>{
@@ -90,7 +69,7 @@ export default new Vuex.Store({
 
     deleteList(context,payload){
       console.log(payload)
-      axios.delete(`http://localhost:3000/api/delete?id=${payload}`)
+      axios.delete(`http://localhost:3000/board/delete?id=${payload}`)
       .then(()=>{
         alert('삭제되었습니다.')
       }).catch(err=>{
@@ -99,7 +78,7 @@ export default new Vuex.Store({
     },
     
     updateProcess(context,payload){
-      axios.get(`http://localhost:3000/api/detailList?id=${payload}`,{payload})
+      axios.get(`http://localhost:3000/board/detailList?id=${payload}`,{payload})
       .then((res)=>{
         context.commit('successDetailList',res.data)
       }).catch(err=>{
@@ -108,20 +87,41 @@ export default new Vuex.Store({
     },
 
     updateList(context,{getId,con}){
-      axios.patch(`http://localhost:3000/api/updatePage?id=${getId}`,{
+      axios.patch(`http://localhost:3000/board/updatePage?id=${getId}`,{
         getId,
         title : this.state.title,
-        writer : this.state.writer,
-        password : this.state.password,
         contents : con
       })
     }
-
-
-
-
-
   },
+
+    SignupProcess(){
+      axios.post('http://localhost:3000/user/signup',{
+        email:this.state.user.email,
+        name:this.state.user.name,
+        password:this.state.user.password
+      }).then(()=>{
+        this.state.isLogin = 'Y';
+        this.state.user.email='';
+        this.state.user.password='';
+      }).catch( (err) =>{
+        console.log(err);
+      })
+    },
+
+    LoginProcess(){
+      axios.post('http://localhost:3000/user/login',{
+        email: this.state.user.email,
+        password:this.state.user.password
+      }).then(()=>{
+        alert('로그인 성공');
+        this.$router.push({name:"Home"})
+      }).catch(err=>{
+        console.log(err);
+      })
+      },
+
+
   modules: {
   }
 })
