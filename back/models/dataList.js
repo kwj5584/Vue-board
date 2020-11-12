fbfa6173cd18ce058138d051c01a2214b35ea06e
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2020-11-12 18:17:15 +0900

    Add files via upload

--- back/models/dataList.js
+++ back/models/dataList.js
@@ -3,11 +3,18 @@ const autoIncrement = require('mongoose-auto-increment')
 
 const Schema = mongoose.Schema;
 const dotenv = require('dotenv')
+
 dotenv.config();
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tudg.mongodb.net/Clusters?retryWrites=true&w=majority`
 
-const connection = mongoose.createConnection(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tudg.mongodb.net/Clusters?retryWrites=true&w=majority`)
+// const boardConnection = mongoose.createConnection(url,(err,database)=>{
+//     if(err){
+//        console.error(err);
+//       }
+//       db = database;
+//   })
+const boardConnection = mongoose.createConnection(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tudg.mongodb.net/Clusters?retryWrites=true&w=majority`);
 
-autoIncrement.initialize(connection);
 
 const dataSchema = new Schema({
   id:{
@@ -17,23 +24,16 @@ const dataSchema = new Schema({
     type : String,
     requried:true
   },
-  writer:{
-    type : String,
-    required:true
-  },
-  password:{
-    type : String,
-    required:true
-  },
   contents:{
     type : String,
     requried:true
-  }
+  },
 })
+
+autoIncrement.initialize(boardConnection);
 
 dataSchema.plugin(autoIncrement.plugin,{
   model : 'dataList',
-  filed:'id',
   startAt:1,
   increment:1
 })
---
+++ back/models/userList.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+const Schema = mongoose.Schema;
+const dotenv = require('dotenv');
+dotenv.config();
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tudg.mongodb.net/Clusters?retryWrites=true&w=majority`
+mongoose.set('useCreateIndex', true)
+const userConnection = mongoose.createConnection(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tudg.mongodb.net/Clusters?retryWrites=true&w=majority`)
+
+const userSchema = new Schema({
+  id:{
+    type:Number
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  name:{
+    type:String,
+    required:true
+  }
+})
+
+autoIncrement.initialize(userConnection);
+
+userSchema.plugin(autoIncrement.plugin,{
+  model:'userList',
+  startAt:1,
+  increment:1
+})
+
+module.exports = mongoose.model('userList', userSchema);
