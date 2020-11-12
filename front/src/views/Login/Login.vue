e68b9ddcb03c6ab9843d73085709b3681f55c6c4
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2020-11-12 18:14:08 +0900

    회원가입,로그인 구성(미완)

---
+++ front/src/views/Login/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="email" placeholder="Enter E-mail" v-model='$store.state.user.email'>
+    <br>
+    <input type="password" placeholder="Enter Password" v-model="$store.state.user.password">
+    <br>
+    
+    <button @click='login'>Login</button>
+    <button @click='back'>reset</button>
+    <button @click="signup">Sign up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    login(){
+      this.$store.dispatch('LoginProcess');
+      
+    },
+    back(){
+      this.$router.push({name:"Home"})
+    },
+    signup(){
+      this.$router.push({name:'Signup'})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/views/Login/Signup.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <label name='E-mail'>
+    <input 
+    type="email"
+    v-model="$store.state.user.email"
+    placeholder="E-mail"
+    required
+    />
+    </label>
+    <br>
+    <input  
+    type='text'
+    v-model="$store.state.user.name"
+    placeholder="Name"
+    required
+    />
+    <br>
+    <input type="password"
+    v-model="$store.state.user.password"
+    placeholder="Password"
+    required
+    >
+    <br>
+    <input type="password"
+    v-model="$store.state.user.passwordConfirm"
+    placeholder="PasswordConfirm"
+    required
+    >
+    <br>
+    <button @click='submit'>SignUp</button>
+    <button @click='back'>Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    submit(){
+      if(this.$store.state.user.password !== this.$store.state.user.passwordConfirm){
+        alert('비밀번호를 확인하세요');
+      }
+      else{
+        this.$store.dispatch('SignupProcess');
+        this.$router.push({name:'Login'})
+      }
+    },
+    back(){
+      this.$router.push({name:"Home"})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
