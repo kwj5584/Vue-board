08e55f9695bc83ce9a090be0427ce4ca5fd965fe
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2020-11-20 16:25:25 +0900

    클릭시 게시물 상세정보

--- front/src/views/board/DetailList.vue
+++ front/src/views/board/DetailList.vue
@@ -1,16 +1,16 @@
 <template>
 <div>
   <div>
-    제목:{{this.$store.state.title}}
+    제목:{{$store.state.title}}
     <br>
-    <!-- 작성자:{{this.$store.state.user.name}} -->
+    작성자:{{$store.state.writer}}
     <br>
   </div>
   <hr>
 <viewer
   height = '500px'
-  v-if='this.$store.state.contents != null' 
-  :initialValue="this.$store.state.contents "
+  v-if='$store.state.contents != null' 
+  :initialValue="$store.state.contents "
 />
 
 <br><hr>
@@ -19,10 +19,10 @@
   <button @click='onDelete'>삭제</button>
   <button @click='onUpdate'>수정하기</button>
 </div>
-<hr><br><br><br><br>
+<hr><br>
 
 <div>
-  <Comments></Comments>
+  <Comments :id="this.$route.query.id"></Comments>
 </div>
 </div>  
 </template>
@@ -39,38 +39,55 @@ export default {
     created(){
       const getId = this.$route.query.id
       this.$store.dispatch('detailPage',getId)
+      console.log('작성자 : ',this.$store.state.writer, '로그인한 사람:',this.$store.state.nowUserInfo.email)
       },
     methods:{
       back(){
       this.$router.push({name:'Home'})
     },
       onDelete(){
-         const passwd = prompt('비밀번호 입력');
+        if(this.$store.state.writer === this.$store.state.nowUserInfo.email){
+          const passwd = prompt('비밀번호 입력');
         const getId = this.$route.query.id
-        if(this.$store.state.user.password === passwd){
+        if(this.$store.state.nowUserInfo.password === passwd){
         this.$store.dispatch('deleteList',getId);
           alert("삭제되었습니다.")
           this.$router.push({name:'Home'})
           window.location.reload();  
         }
-        else{ alert('비밀번호가 다릅니다.');}
+        else{ 
+          alert('비밀번호가 다릅니다.');
+          }
+        }
+        else{
+          alert('접근 권한이 없습니다.')
+        }
        },
 
       onUpdate(){
-        const passwd = prompt('비밀번호를 입력하세요');
-        if(this.$store.state.user.password === passwd){
-        this.$router.push({name : 'UpdatePage', query: { id: this.$route.query.id }})
-       }
-       else{
+        if(this.$store.state.writer===this.$store.state.nowUserInfo.email){
+          const passwd = prompt('비밀번호를 입력하세요');
+          if(this.$store.state.user.password === passwd){
+          this.$router.push({name : 'UpdatePage', query: { id: this.$route.query.id }})
+          }
+        else{
          alert('비밀번호가 다릅니다.');
          window.location.reload();
       //  }
       }
+        }else{
+          alert('접근 권한이 없습니다.')
+        }
+        
     },
     }
 };
 </script>
 
 <style>
-
+.center{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
